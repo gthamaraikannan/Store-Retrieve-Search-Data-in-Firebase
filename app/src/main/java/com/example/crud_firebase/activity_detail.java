6ee1585794014e7aee8d3e1f8ab676eafdc20889
gthamaraikannan <115413999+gthamaraikannan@users.noreply.github.com>
2023-03-18 21:15:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/crud_firebase/activity_detail.java
@@ -0,0 +1,37 @@
+package com.example.crud_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class activity_detail extends AppCompatActivity {
+
+    TextView detailDesc, detailTitle, detailLang;
+
+    ImageView detailImage;
+    String key = "";
+    String imageUrl = "";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        detailDesc = findViewById(R.id.detailDesc);
+        detailImage = findViewById(R.id.detailImage);
+        detailTitle = findViewById(R.id.detailTitle);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null){
+            detailDesc.setText(bundle.getString("Description"));
+            detailTitle.setText(bundle.getString("Title"));
+            detailLang.setText(bundle.getString("Language"));
+            key = bundle.getString("Key");
+            imageUrl = bundle.getString("Image");
+            Glide.with(this).load(bundle.getString("Image")).into(detailImage);
+        }
+    }
+}
